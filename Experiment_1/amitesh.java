acc47d486a925e25e10fe61ae63ce61f4227fc44
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-09-09 22:34:22 +0530

    Add files via upload

---
+++ Experiment_1/amitesh.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class First{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the employee name: ");
+        String empName = sc.nextLine();
+        System.out.println("Enter the employee no: ");
+        int empNo = sc.nextInt();
+        System.out.println("Enter the Basic: ");
+        int empBasic = sc.nextInt();
+
+        double DA = 0.7* empBasic;
+        System.out.println("The DA is "+DA);
+
+        double HRA = 0.3* empBasic;
+        System.out.println("The DA is "+HRA);
+
+        double PF = 0.1* empBasic;
+        System.out.println("PF is: "+PF);
+    }
+}
---
+++ Experiment_1/amitesh4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class amitesh4 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the age 1: ");
+        int age1 = sc.nextInt();
+        System.out.print("Enter the age 2: ");
+        int age2 = sc.nextInt();
+        System.out.print("Enter the age 3: ");
+        int age3 = sc.nextInt();
+        int oldest = Math.max(age1, Math.max(age2, age3));
+        int youngest = Math.min(age1, Math.min(age2, age3));
+        System.out.println("The oldest person is " + oldest + " years old.");
+        System.out.println("The youngest person is " + youngest + " years old.");
+    }
+}
