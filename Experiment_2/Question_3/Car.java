0b6102079c61feb977a9aa7202ff9896ba37da98
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-09-09 22:38:27 +0530

    Add files via upload

---
+++ Experiment_2/Question_3/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    private String brand;
+
+    // Constructor to initialize the brand field
+    public Car() {
+        this.brand = "Ford";
+    }
+
+    // Getter method for the brand field
+    public String getBrand() {
+        return brand;
+    }
+}
---
+++ Experiment_2/Question_3/Sample.java
@@ -0,0 +1,12 @@
+public class Sample {
+    public static void main(String[] args) {
+        // Create an instance of the Car class
+        Car myCar = new Car();
+
+        // Call the getBrand() method and store the value in a variable
+        String carBrand = myCar.getBrand();
+
+        // Print the value of the brand
+        System.out.println("Brand of the car: " + carBrand);
+    }
+}
