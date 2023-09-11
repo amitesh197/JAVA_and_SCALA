a63c254366358e80c4a7449e3b8f474495da6494
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-09-11 19:46:27 +0530

    Add files via upload

---
+++ Experiment_2/Question_1/mypackage/UserDefinedPackage.java
@@ -0,0 +1,8 @@
+// UserDefinedPackage.java
+package mypackage;
+
+public class UserDefinedPackage {
+    public static void printMessage() {
+        System.out.println("Hello, user! This message is from the user-defined package.");
+    }
+}
