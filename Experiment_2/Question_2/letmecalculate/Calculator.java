4dd21e925b14118264419bc043e9297088cf3f20
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-09-11 19:47:34 +0530

    Add files via upload

---
+++ Experiment_2/Question_2/letmecalculate/Calculator.java
@@ -0,0 +1,23 @@
+package letmecalculate;
+
+public class Calculator {
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        if (b == 0) {
+            System.out.println("Division by zero is not allowed.");
+            return Double.NaN; // Not-a-Number to indicate an error
+        }
+        return (double) a / b;
+    }
+}
