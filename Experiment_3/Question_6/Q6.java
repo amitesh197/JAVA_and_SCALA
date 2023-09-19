ae024d0f6a837b0ac0af928e14d90ed6331c18af
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-09-19 10:41:29 +0530

    Add files via upload

---
+++ Experiment_3/Question_6/Q6.java
@@ -0,0 +1,25 @@
+public class Q6 {
+    public static void main(String[] args) {
+        String s1 = "geeks";
+        System.out.println("First string is: " + s1);
+        String s2 = "keegs";
+        System.out.println("Second string is: " + s2);
+
+        int z = 0;
+        int xor = 0;
+
+        for (int i = 0; i < s1.length(); i++){
+             xor ^= s1.charAt(i)^s2.charAt(i);
+         
+        }
+
+        if(xor == 0) { System.out.println("yes ") ;}
+        else{ System.out.println("no");}
+
+     
+        if(z>0){
+        System.out.print("They are Meta Strings!");
+        }
+    }
+}
+
