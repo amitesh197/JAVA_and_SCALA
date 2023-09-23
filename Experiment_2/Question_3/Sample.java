c954258506190a55731c129bae8cee1b2485b726
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-09-23 11:07:52 +0530

    Update Sample.java

--- Experiment_2/Question_3/Sample.java
+++ Experiment_2/Question_3/Sample.java
@@ -1,12 +1,10 @@
 public class Sample {
     public static void main(String[] args) {
-        // Create an instance of the Car class
+        
         Car myCar = new Car();
 
-        // Call the getBrand() method and store the value in a variable
         String carBrand = myCar.getBrand();
 
-        // Print the value of the brand
         System.out.println("Brand of the car: " + carBrand);
     }
 }
