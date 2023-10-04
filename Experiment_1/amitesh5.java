1efeef9be5a28281d55f8364f87a8e6b04285da3
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-10-04 19:55:45 +0530

    Update amitesh5.java

--- Experiment_1/amitesh5.java
+++ Experiment_1/amitesh5.java
@@ -5,12 +5,16 @@ class amitesh5{
         int x=2, y=5, z=0;
         boolean a = (x==2);
         System.out.println(a);
+        
         boolean b = (x!=5);
         System.out.println(b);
+        
         boolean c = (x!=5 && y>=5);
         System.out.println(c);
+        
         boolean d = (z!=0 || x==2);
         System.out.println(d);
+        
         boolean e = (!(y<10));
         System.out.println(e);
     }
