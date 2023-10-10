8104ed7f22fd6cad49abd24a60dcac7d2d72d9f3
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-10-10 18:54:19 +0530

    Add files via upload

---
+++ Experiment_5/Q6.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized void decrement() {
+        count--;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
+
+class IncrementThread extends Thread {
+    private Counter counter;
+
+    public IncrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            counter.increment();
+            System.out.println("Incremented: " + counter.getCount());
+        }
+    }
+}
+
+class DecrementThread extends Thread {
+    private Counter counter;
+
+    public DecrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            counter.decrement();
+            System.out.println("Decremented: " + counter.getCount());
+        }
+    }
+}
+
+public class Q6 {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        IncrementThread incrementThread = new IncrementThread(counter);
+        DecrementThread decrementThread = new DecrementThread(counter);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Press Enter to start the threads...");
+        scanner.nextLine();
+
+        incrementThread.start();
+        decrementThread.start();
+
+        try {
+            incrementThread.join();
+            decrementThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Count: " + counter.getCount());
+    }
+}
