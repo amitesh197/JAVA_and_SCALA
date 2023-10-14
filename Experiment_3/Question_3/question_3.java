0b120a86e65493c927c5b2514da64c285d9a15b0
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-10-14 10:42:33 +0530

    Update question_3.java

--- Experiment_3/Question_3/question_3.java
+++ Experiment_3/Question_3/question_3.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class  question_3 {
+public class  question_3 
+{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no. of tests: ");
