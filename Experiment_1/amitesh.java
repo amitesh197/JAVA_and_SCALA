200902520bc3224bc2521d91d0e1d90198b9fcfb
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-10-26 10:18:40 +0530

    Update amitesh.java

--- Experiment_1/amitesh.java
+++ Experiment_1/amitesh.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-class First{
+class First
+{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the employee name: ");
