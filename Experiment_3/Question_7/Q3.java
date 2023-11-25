a83221f030fd7681d36748997e906335bba33532
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-11-25 23:01:15 +0530

    Update Q3.java

--- Experiment_3/Question_7/Q3.java
+++ Experiment_3/Question_7/Q3.java
@@ -1,5 +1,6 @@
 public class Q3 {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
     String string = "This is a Java and Scala Laboratory!!";
 
     int upperCaseCount = 0;
