509fc5ec3a8ba92b224d070f6150239bd5048c1d
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-12-13 19:45:16 +0530

    Add files via upload

---
+++ EXPERIMENTS/JAVA/Marks_Out_Of_Bound_Exception.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import MyErrorClasses.MyErrorClass;
+public class Marks_Out_Of_Bound_Exception{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int seatno, center_number, sem1, sem2, sem3; 
+        String date = new String();
+        String name = new String();
+        System.out.println("Enter the Name: ");
+        name = scanner.nextLine();
+        System.out.println("Enter the seat number: ");
+        seatno = scanner.nextInt();
+        System.out.println("Enter the Date: ");
+        date = scanner.nextLine();
+        System.out.println("Enter the center number: ");
+        center_number = scanner.nextInt();
+        System.out.println("Enter the sem1 marks: ");
+        sem1 = scanner.nextInt();
+        System.out.println("Enter the sem2 marks: ");
+        sem2 = scanner.nextInt();
+        System.out.println("Enter the sem3 marks: ");
+        sem3 = scanner.nextInt();
+
+        try{
+            if(sem1>100 || sem1<0 || sem2>100 || sem2<0 || sem3>100 || sem3<0){
+                throw new MyErrorClass("Error: marks out of bound!");
+            }
+        }
+        catch(MyErrorClass exp){
+            System.out.println("INVALID MARKS!");
+        }
+
+
+
+    };
+};
+
---
+++ EXPERIMENTS/JAVA/Multithreading_Tables.java
@@ -0,0 +1,37 @@
+class four extends Thread{
+    public void run(){
+        System.out.println("Table of 4: ");
+        for(int i=1; i<=10; i++){
+            System.out.println(4 + "*" + i + "=" + 4*i);
+        }
+    }
+}
+
+class five extends Thread{
+    public void run(){
+        System.out.println("Table of 5: ");
+        for(int i=1; i<=10; i++){
+            System.out.println(5 + "*" + i + "=" + 5*i);
+        }
+    }
+}
+
+class thirteen extends Thread{
+    public void run(){
+        System.out.println("Table of 13: ");
+        for(int i=1; i<=10; i++){
+            System.out.println(13 + "*" + i + "=" + 13*i);
+        }
+    }
+}
+
+class Multithreading_Tables{
+    public static void main(String args[]){
+        four t1 = new four();
+        five t2 = new five();
+        thirteen t3 = new thirteen();
+        t1.start();
+        t2.start();
+        t3.start();
+        }
+}
