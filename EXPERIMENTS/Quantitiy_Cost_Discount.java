6f18bfe6dc6362e0b57d66af9beb4b94373c2245
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-12-13 19:27:30 +0530

    Add files via upload

---
+++ EXPERIMENTS/Quantitiy_Cost_Discount.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Quantitiy_Cost_Discount {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);   
+        int quant, cost=100;
+        double total_cost=0;
+        System.out.println("enter the quantity: ");
+        quant = sc.nextInt();
+        total_cost = cost * quant;
+        if(total_cost > 1000){
+            total_cost = total_cost * 0.9;
+        }
+        System.out.println("Total cost is: " + total_cost);
+}}
+
