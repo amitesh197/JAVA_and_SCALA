2d5657a67eaf0d6ffb915edfa1db402714877170
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-12-13 19:38:40 +0530

    Add files via upload

---
+++ EXPERIMENTS/JAVA/Sum_of_Array_Except_index_number.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class  Sum_of_Array_Except_index_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of tests: ");
+        int T = sc.nextInt(); 
+
+        while (T-- > 0) {
+            System.out.println("Enter the length of the array: ");
+            int N = sc.nextInt(); 
+            int[] arr = new int[N];
+            long[] result = new long[N]; 
+
+            System.out.println("Enter the array: ");
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            long totalSum = 0;
+            for (int i = 0; i < N; i++) {
+                totalSum += arr[i];
+            }
+
+            for (int i = 0; i < N; i++) {
+                result[i] = totalSum - arr[i];
+            }
+
+            for (int i = 0; i < N; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
