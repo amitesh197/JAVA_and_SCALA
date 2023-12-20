a3fdde8dfc6f55122b131d0970b079c806051e53
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2023-12-20 11:26:40 +0530

    Update Q2.java

--- Experiment_5/Q2.java
+++ Experiment_5/Q2.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Q2 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         int n;
         System.out.print("Enter the number of integers: ");
