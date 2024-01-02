056b503a11471b017f16410e42aa3b67615da49a
Amitesh Sawarkar <123076729+amitesh197@users.noreply.github.com>
2024-01-02 11:01:43 +0530

    Update question_1.java

--- Experiment_3/Question_1/question_1.java
+++ Experiment_3/Question_1/question_1.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class question_1 
 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         int number=0;
         
